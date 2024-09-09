a22afa2b490073d780ba60656392f7b22544fab8
datluu <tad.louis2810@gmail.com>
2024-09-09 18:42:04 +1000

    add navbar and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './section/Navbar'
+
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
-      <h1 className="text-2xl text-white underline">Hel</h1>
+      <Navbar />
     </main>
   )
 }
---
+++ src/section/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+        <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-center py-5 mx-auto c-space">
+                <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                    David
+                </a>
+
+                <button>
+                    <img src={"assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+                </button>
+            </div>
+        </div>
+    </header>
+  
+)
+}
+
+export default Navbar
